65c26ae1d8dfb7c46f52b88e030453ff7def457b
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2026-02-21 11:39:55 +0530

    Add solution for relu-activation

---
+++ relu-activation/relu-activation.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def relu(x):
+    """
+    Implement ReLU activation function.
+    """
+    # Write code here
+    x = np.asarray(x, dtype=float)
+
+    result = np.maximum(0, x)
+
+    return result
