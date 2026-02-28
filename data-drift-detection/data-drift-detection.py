694fb0192ad1b9618121ef7da69701005f5e2e16
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2026-02-28 11:59:45 +0530

    Add solution for data-drift-detection

---
+++ data-drift-detection/data-drift-detection.py
@@ -0,0 +1,19 @@
+def detect_drift(reference_counts, production_counts, threshold):
+    """
+    Compare reference and production distributions to detect data drift.
+    """
+    # Write code here
+    ref_total = sum(reference_counts)
+    prod_total = sum(production_counts)
+
+    ref_probs = [c / ref_total for c in reference_counts]
+    prod_probs = [c / prod_total for c in production_counts]
+
+    tvd = 0.5 * sum(abs(p - q) for p,q in zip(ref_probs, prod_probs))
+
+    drift_detect = tvd > threshold
+
+    return {
+        'score':tvd,
+        "drift_detected":drift_detect
+    }
