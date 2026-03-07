8c27066ae7661797e75162299e42861b9c69b94e
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2026-03-07 08:02:21 +0530

    Add solution for image-histogram

---
+++ image-histogram/image-histogram.py
@@ -0,0 +1,12 @@
+def image_histogram(image):
+    """
+    Compute the intensity histogram of a grayscale image.
+    """
+    # Write code here
+    hist = [0] * 256
+
+    for row in image:
+        for pixel in row:
+            hist[pixel] += 1
+
+    return hist
