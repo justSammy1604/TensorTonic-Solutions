313c8e52f2e357f3b058f3f3383b571bb3dbf4d0
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2026-04-10 06:10:47 +0530

    Add solution for cumulative-returns

---
+++ cumulative-returns/cumulative-returns.py
@@ -0,0 +1,12 @@
+def cumulative_returns(returns):
+    """
+    Compute the cumulative return at each time step.
+    """
+    result = []
+    wealth = 1.0
+
+    for r in returns:
+        wealth *= (1 + r)
+        result.append(wealth - 1)
+
+    return result
